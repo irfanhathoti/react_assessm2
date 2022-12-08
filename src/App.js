4e79c2bc176152abcd50016376186a92ddbddcf8
irfan.hathoti <irfan.hathoti@htcinc.com>
2022-12-08 22:42:35 +0530

    'first'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import ListEmployee from './Components/ListEmployee';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import EmployeeData from './Components/EmployeeData';
+import viewEmployee from './Components/ViewEmployee';
+import UserList from './exerciseOne/UserList'
+// import UserList from './exerciseOne/UserList';
+import Reminder from './Components/Reminder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <UserList /> */}
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={ListEmployee}></Route>
+            <Route path="/employees" component={ListEmployee}></Route>
+            <Route path="/add-employee/:id" component={EmployeeData}></Route>
+
+            {/* here iam matchin the id that employee detail will open */}
+
+            <Route path="/view-employee/:id" component={viewEmployee}></Route>
+          </Switch>
+        </div>
+      </Router>
+
+<Reminder/>
+
     </div>
   );
 }
